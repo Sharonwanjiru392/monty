b28b599fd89584f9df91b7982dae4411c2ee678e
Listus97 <namurendecallistus@gmail.com>
2023-06-21 16:14:07 -0700

    free stack

---
+++ free_stack.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * free_stack - frees a doubly linked list
+ * @head: head of the stack
+ * 
+ * Description:
+ * This function frees the memory occupied by a doubly linked list.
+ *
+ * Return: void
+ */
+
+void free_stack(stack_t *head)
+{
+	stack_t *aux;
+
+	aux = head;
+	while (head)
+	{
+		aux = head->next;
+		free(head);
+		head = aux;
+	}
+}
