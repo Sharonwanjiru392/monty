4ffbbf6cdf9c9b166ac2db1ca023e29bd9d74757
Listus97 <namurendecallistus@gmail.com>
2023-06-21 15:57:47 -0700

    stack

---
+++ stack.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * f_stack - Prints the top element of the stack
+ * @head: Pointer to the head of the stack
+ * @counter: Line number
+ *
+ * Description: This function prints the top element of the stack
+ *
+ * Return: No return value
+ */
+
+void f_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
